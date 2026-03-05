9772348caaa351569759a9623a653a5b57c2d9ae
Larrynuhuh <swastiksingh1402@gmail.com>
2026-03-05 18:47:47 +0530

    added metrics to topology too

--- manifolds/topology.py
+++ manifolds/topology.py
@@ -3,54 +3,55 @@ import jax
 import jax.numpy as jnp
 from manifolds import vectors as vct
 from geoutils import Vector, Matrix, Scalar, Tensor
+from basis import metrics as mtc
 
 @jax.jit
-def linlen(l: Matrix) -> Scalar:
+def linlen(g: Matrix, l: Matrix) -> Scalar:
     
     diff = jnp.diff(l, axis = 0)
-    lens = jnp.linalg.norm(diff, axis = 1)
+    lens = mtc.norm(g, diff)
     sums = jnp.sum(lens)
 
     return sums
 
 
-def midp(p1: Vector, p2: Vector) -> Vector:
+def midp(g: Matrix, p1: Vector, p2: Vector) -> Vector:
     mid = (p1 + p2) / 2.0
     return mid
 
 @jax.jit
-def vmidp(p1: Matrix, p2: Matrix) -> Matrix:
-    return jax.vmap(midp, in_axes=(0,0))(p1, p2)
+def xmidp(g: Matrix, p1: Matrix, p2: Matrix) -> Matrix:
+    return jax.vmap(midp, in_axes=(0,0,0))(g, p1, p2)
 
 
 # to check distance of point from line
 
 @jax.jit
-def segdist(f, g, pt):
+def segdist(g: Matrix, f, g, pt):
     v = g-f
     w = pt-f
-    t = us.div(jnp.dot(w,v),jnp.dot(v,v))
+    t = us.div(mtc.iprod(g,w,v),mtc.iprod(g,v,v))
     tc = jnp.clip(t, 0, 1)
 
     cp = f + tc * v
-    dist = jnp.linalg.norm(pt - cp)
+    dist = mtc.norm(pt - cp)
 
     return dist
 
 # USER CALLS PLDIST
 @jax.jit
-def pldist(l: Matrix, pt: Vector) -> Scalar:
+def pldist(g: Matrix, l: Matrix, pt: Vector) -> Scalar:
     a = l[:-1]
     b = l[1:]
 
-    curve_dist = jax.vmap(segdist, in_axes = (0, 0, None))
-    summed = curve_dist(a, b, pt)
+    curve_dist = jax.vmap(segdist, in_axes = (None, 0, 0, None))
+    summed = curve_dist(g, a, b, pt)
 
     return jnp.min(summed)
 
 @jax.jit
-def vpldist(l: Tensor, pt: Matrix) -> Vector:
-    return jax.vmap(pldist, in_axes = (0, 0))(l, pt)
+def xpldist(l: Tensor, pt: Matrix) -> Vector:
+    return jax.vmap(pldist, in_axes = (0, 0, 0))(g, l, pt)
 
 @jax.jit
 def sdf(l: Matrix, pt: Vector) -> Scalar:
